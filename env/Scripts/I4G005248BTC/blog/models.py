0ff0872393aad06295d38a50fd38d51b33cfa68a
speedbuster <70118650+speedbuster@users.noreply.github.com>
2022-06-25 18:25:47 -0800

    commit django crud

--- env/Scripts/I4G005248BTC/blog/models.py
+++ env/Scripts/I4G005248BTC/blog/models.py
@@ -1,14 +1,44 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.urls import reverse
+from django.template.defaultfilters import slugify
 # Create your models here.
-# STATUS = (
-#     (0,"Draft"),
-#     (1,"Publish")
-# )
 
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    author = models.ForeignKey(get_user_model())
-    created_on = models.DateTimeField(auto_now_add=True)
-    published = models.DateTimeField()
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published")
+    )
+
+    # DB Fields
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=300, unique=True, editable=False)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="draft"
+    )
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        pass 
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"slug": self.slug})
